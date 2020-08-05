3570ae3ab4ed787f8a7bb4b05379e4ad02294839
Stephen <55206344+spwhittaker@users.noreply.github.com>
2020-08-05 13:06:02 +0100

    Create quickFib.js

---
+++ quickFib.js
@@ -0,0 +1,17 @@
+let fibs = n => {
+
+  if (n === 0 || n === 1) { return 1 }
+  const fibNums = [1, 1];
+
+  for (let i = 1; i < n; i++) {
+    if (!fibNums[i + 1]) {fibNums[i + 1] = fibNums[i - 1] + fibNums[i] }
+
+  }
+  
+  return fibNums[n - 1]
+
+}
+
+console.time('timer')
+console.log(fibs(1476))
+console.timeEnd('timer')
