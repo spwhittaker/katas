39c9e99ed4c97463a87c54b0bd4d95a181ba30c2
Stephen <55206344+spwhittaker@users.noreply.github.com>
2020-08-05 13:18:52 +0100

    Update quickFib.js

--- quickFib.js
+++ quickFib.js
@@ -3,10 +3,10 @@ const fibs = n => {
     if (n === 0 || n === 1) {
         return 1
     }
-    const fibNums = [1, 1];
+    const fibNums = [1, 1]
 
     for (let i = 1; i < n; i++) {
-        fibNums[i + 1] = fibNums[i - 1] + fibNums[i]
+        fibNums.push(fibNums[i - 1] + fibNums[i])
     }
 
     return fibNums[n - 1]
